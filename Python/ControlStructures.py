981a476b1814c658c334e8bf53f8784398c316b1
Ariel <a.santiagocsh@gmail.com>
2025-11-21 08:03:51 -0600

    Python data structures and control structures

---
+++ Python/ControlStructures.py
@@ -0,0 +1,20 @@
+#Conditionals
+a = 1
+b = 2
+if a < b:
+    print("A is greater than B")
+#Loops
+#For loops
+for x in range(1, 10):
+    print(x)
+
+#While loops
+i = 1
+while i < 6:
+    print("I'm young")
+    i += 1
+
+#Functions
+#The function structures as follows, first, they use the def keyword and after that the name of the function followed by parenthesis and double-dot
+def funcion():
+    print("I'm a function")
